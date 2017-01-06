fd9768cf2f6f63b67fe375ebbfc19669a80ab2c4
Lewis Stevens <lewis.stevens07@gmail.com>
2017-01-06 17:05:35 +0000

    Add lepbase Copyright.pm and remove tardibase Copyright.pm

--- modules/EnsEMBL/Web/Document/Element/Copyright.pm
+++ modules/EnsEMBL/Web/Document/Element/Copyright.pm
@@ -36,19 +36,24 @@ sub content {
   my $sd = $self->species_defs;
 
 ## BEGIN CAENSEMBL MODIFICATIONS...
+my $site_name = $self->hub->species_defs->ENSEMBL_SITE_NAME_SHORT;
+my $site_version = $self->hub->species_defs->SITE_RELEASE_VERSION;
+my $site_date = $self->hub->species_defs->SITE_RELEASE_DATE;
 my $html = '<div class=lb-ackn-logos>';
-$html .= '<a href="http://www.ed.ac.uk"><img title="University of Edinburgh" class="lb-footer-logo" src="/img/edinburgh_logo.png"></a>';
+$html .= '<a href="http://ed.ac.uk"><img title="University of Edinburgh" class="lb-footer-logo" src="/img/edinburgh_logo.png"></a>';
+
 $html .= '</div>';
 
   return sprintf( qq(
   <div class="column-two left">
-		   %s release %s - %s -
-		  caenorhabditis.org &copy; <span class="print_hide"><a href="http://www.ed.ac.uk/" style="white-space:nowrap">Edinburgh University</a> / EnsEMBL &copy; <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a></span>
-      <span class="screen_hide_inline">caenorhabditis.org</span> %s
-  </div>),     'caenorhabditis.org", "2", "January 2017", $html
+                   %s release %s - %s -
+                  %s &copy; <span class="print_hide"><a href="http://www.ed.ac.uk/" style="white-space:nowrap">Edinburgh University</a> / EnsEMBL &copy; <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a></span>
+      %s
+  </div>),     $site_name, $site_version, $site_date, $site_name, $html
 ## ...END CAENSEMBL MODIFICATIONS
-	       );
+               );
 
 }
 
 1;
+
