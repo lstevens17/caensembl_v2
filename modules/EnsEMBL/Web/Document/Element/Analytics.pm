24b05b45e9f493a9fc2ddfff8cf399eae30c2a5a
Lewis Stevens <lewis.stevens07@gmail.com>
2017-01-19 19:55:13 +0000

    Add R. Challis' Analytics.pm file to fix tracking

--- modules/EnsEMBL/Web/Document/Element/Analytics.pm
+++ modules/EnsEMBL/Web/Document/Element/Analytics.pm
@@ -32,18 +32,17 @@ sub new {
 sub content {
   my $self = shift;
   my $content;
-
+  my $analytics_code = $self->hub->species_defs->ANALYTICS_CODE;
   $content .= qq{
-	<script>
+        <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-90618508-1', 'auto');
+  ga('create', $analytics_code, 'auto');
   ga('send', 'pageview');
-
-	</script>
+        </script>
   };
 
   return $content;
@@ -52,4 +51,3 @@ sub content {
 
 
 1;
-
